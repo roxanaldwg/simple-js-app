6c7126c1312f272fd4040991b3e910b4683f59dc
roxanaldwg <roxana_ludwig@yahoo.de>
2021-11-07 21:21:18 +0100

    deleted previous content from JS and then added pokemon array
     Added pokemon array
    
    5 pokemons added with their respective name, height and types

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-alert('Hello world');
-
-let favoriteFood = "apple";
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: 'Venusaur',
+    height: 2,
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Beedrill',
+    height: 1,
+    types: ['bug', 'poison']
+  },
+  {
+    name: 'Butterfree',
+    height: 1.1,
+    types: ['bug', 'flying']
+  },
+  {
+    name: 'Sandshrew',
+    height: 0.6,
+    types: ['ground']
+  },
+  {
+    name: 'Nidoran',
+    height: 0.4,
+    types: ['poison']
+  }
+];
