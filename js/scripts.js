2cdf5debadac515fc895d57ca4016588be88b761
roxanaldwg <roxana_ludwig@yahoo.de>
2021-11-19 16:11:33 +0100

    add IIFE with keys "getAll" and "add"
    - wrap pokemonList array in an IIFE
    - added keys getAll and add
    - update the forEach loop code to cope with getAll() and defined data to new variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,65 @@
 //Pokemon List
-let pokemonList = [
-  {
-    name: 'Venusaur',
-    height: 2,
-    types: ['grass', 'poison']
-  },
-  {
-    name: 'Beedrill',
-    height: 1,
-    types: ['bug', 'poison']
-  },
-  {
-    name: 'Butterfree',
-    height: 1.1,
-    types: ['bug', 'flying']
-  },
-  {
-    name: 'Sandshrew',
-    height: 0.6,
-    types: ['ground']
-  },
-  {
-    name: 'Nidoran',
-    height: 0.4,
-    types: ['poison']
+
+//put pokemonList in IIFE
+
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: 'Venusaur',
+      height: 2,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Beedrill',
+      height: 1,
+      types: ['bug', 'poison']
+    },
+    {
+      name: 'Butterfree',
+      height: 1.1,
+      types: ['bug', 'flying']
+    },
+    {
+      name: 'Sandshrew',
+      height: 0.6,
+      types: ['ground']
+    },
+    {
+      name: 'Nidoran',
+      height: 0.4,
+      types: ['poison']
+    }
+  ];
+  //returns the pokemonList array
+  function getAll() {
+    return pokemonList;
   }
-];
+
+    //adds new data to the pokemonList array
+  function add(pokemon) {
+      pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+
+})();
 
 //Replaced for loop - for each to list the name and height of the pokemons:
 
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
+//define data to new variables using getAll()
+  let pokemonName = pokemon.name
+  let pokemonHeight = pokemon.height
+  let pokemonType = pokemon.types
+
+//Write to the document
   document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + 'm)' );
 
 //add a conditional to highlight the tallest Pokemon in the list
 
-if (pokemon.height >= 2)
+  if (pokemon.height >= 2)
   document.write(" Wow, that's big!");
 });
